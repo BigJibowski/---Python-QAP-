d7c89d8d4067d6b062671d9921c7bcb0adc9c6ff
BigJibowski <vk_kvch@inbox.ru>
2021-04-01 16:42:04 +0300

    Delete 12.7.3.py

--- 12.7.3.py
+++
@@ -1,8 +0,0 @@
-money = float(input('Введите сумму '))
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = []
-for i in per_cent.values():
-    deposit.append(i*money/100)
-print('Ваши деньги: ', money)
-print(deposit)
-print('Максимальная сумма, которую вы можете заработать — ', max(deposit))
